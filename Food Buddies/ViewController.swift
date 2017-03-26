1144f655549862c7d1e05440101abcd3ddf7f8ef
Mikko Manoza <josephmikkomanoza@gmail.com>
2017-03-26 18:40:53 +0800

    Initial Commit

---
+++ Food Buddies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Food Buddies
+//
+//  Created by John Paul Manoza on 26/03/2017.
+//  Copyright © 2017 Nostalgic Kid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
